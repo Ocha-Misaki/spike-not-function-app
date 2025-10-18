cdec8c755424161a9c09cf095f7c935e77aae17d
Ocha-Misaki <himawari.158fuji@gmail.com>
2025-10-18 22:41:52 +0900

    コメントを削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   def index
-    # エラー: @postではなく@postsであるべき
     @post = Post.all
   end
 
@@ -9,22 +8,18 @@ class PostsController < ApplicationController
   end
 
   def create
-    # エラー1: paramsの取得方法が間違っている（strong parametersを使っていない）
     @post = Post.new(params[:post])
 
-    # エラー2: saveの結果を確認していない、常にshowにリダイレクト
     @post.save
     redirect_to post_path(@post)
   end
 
   def show
-    # エラー: findの引数が間違っている（params[:id]であるべき）
     @post = Post.find(params[:post_id])
   end
 
   private
 
-  # このメソッドは定義されているが、createアクションで使われていない
   def post_params
     params.require(:post).permit(:title, :content)
   end
